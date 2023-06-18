6e28d760b4d45574e248363c14ed32aa00ef58b6
Josue Alamo <githubpersonalgit@gmail.com>
2023-06-18 02:53:38 -0500

    Players are now initialized with a starter deck

--- rps.py
+++ rps.py
@@ -11,9 +11,46 @@ class Player:
 
     def __init__(self, name):
         self._name = name
-        self._deck = []
+        self._deck = self.create_starter_deck()
         self._hand = []
         self._discard_pile = []
+        
+    def create_starter_deck(self):
+        """Creates a starter deck for all players."""
+        card1 = Card("pebble", 1, 3, "rock")
+        card2 = Card("pebble", 2, 2, "rock")
+        card3 = Card("pebble", 4, 3, "rock")
+        card4 = Card("pebble", 3, 2, "rock")
+        card5 = Card("pebble", 2, 3, "rock")
+        card6 = Card("safety scissors", 4, 1, "scissors")
+        card7 = Card("safety scissors", 3, 2, "scissors")
+        card8 = Card("safety scissors", 5, 1, "scissors")
+        card9 = Card("safety scissors", 4, 3, "scissors")
+        card10 = Card("safety scissors", 3, 1, "scissors")
+        card11 = Card("sticky note", 1, 3, "paper")
+        card12 = Card("sticky note", 2, 4, "paper")
+        card13 = Card("sticky note", 1, 5, "paper")
+        card14 = Card("sticky note", 2, 3, "paper")
+        card15 = Card("sticky note", 1, 4, "paper")
+        starter_deck = [
+            card1,
+            card2,
+            card3,
+            card4,
+            card5,
+            card6,
+            card7,
+            card8,
+            card9,
+            card10,
+            card11,
+            card12,
+            card13,
+            card14,
+            card15,
+        ]
+        
+        return starter_deck
 
     def get_name(self):
         """Rerturns the player's name."""
@@ -62,41 +99,8 @@ class RPS:
 
     def __init__(self):
         """Initializes the game with the given player 1 and player 2 boards."""
-
         self._player1_board = ["*", ".", ".", ".", "*"]
         self._player2_board = ["*", ".", ".", ".", "*"]
-        card1 = Card("pebble", 1, 3, "rock")
-        card2 = Card("pebble", 2, 2, "rock")
-        card3 = Card("pebble", 4, 3, "rock")
-        card4 = Card("pebble", 3, 2, "rock")
-        card5 = Card("pebble", 2, 3, "rock")
-        card6 = Card("safety scissors", 4, 1, "scissors")
-        card7 = Card("safety scissors", 3, 2, "scissors")
-        card8 = Card("safety scissors", 5, 1, "scissors")
-        card9 = Card("safety scissors", 4, 3, "scissors")
-        card10 = Card("safety scissors", 3, 1, "scissors")
-        card11 = Card("sticky note", 1, 3, "paper")
-        card12 = Card("sticky note", 2, 4, "paper")
-        card13 = Card("sticky note", 1, 5, "paper")
-        card14 = Card("sticky note", 2, 3, "paper")
-        card15 = Card("sticky note", 1, 4, "paper")
-        self._starter_deck = [
-            card1,
-            card2,
-            card3,
-            card4,
-            card5,
-            card6,
-            card7,
-            card8,
-            card9,
-            card10,
-            card11,
-            card12,
-            card13,
-            card14,
-            card15,
-        ]
 
     def get_player1_board(self):
         """Returns the player 1's board."""
@@ -110,6 +114,11 @@ class RPS:
         """Shuffle the	discard pile."""
         random.shuffle(discard_pile)
         return discard_pile
+    
+    def play_game(self, player):
+        """Allows the player to play the game and place cards on the board."""
+        
+        
 
     def confirm_cards(self, player):
         """Confirms the player's played cards."""
